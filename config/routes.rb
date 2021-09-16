d106448bda9267d0f181704d749a29e41bdfa625
vgoylo <y_draiv.by@mail.ru>
2021-09-16 09:55:48 +0300

    Add micropost

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   get 'goodbye_123', to: 'application#goodbye'
   root 'users#index'
-
- 
 end
